5782f483a815041ef428fc3f025b7bc0f77e2eb7
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-12-03 00:42:14 +0800

    Update carcontroller.py

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -46,7 +46,7 @@ def get_long_tune(CP, params):
   kdV = [0.]
   if CP.carFingerprint in TSS2_CAR:
     kiBP = [0.,   0.2,   1.,   5.,   14.,   20.,   27.]
-    kiV = [0.34,  0.35,  0.33, 0.22, 0.215,  0.17, 0.10]
+    kiV = [0.33,  0.35,  0.33, 0.22, 0.215,  0.17, 0.10]
     kdV = [0.25 / 3]
 
     # Since we compensate for imprecise acceleration in carcontroller and error correct on aEgo, we can avoid using gains
